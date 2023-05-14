4545dd9aec7a30c92ab32d7f33010c2196b7e920
Reda AGAOUA <reda.agaoua@gmail.com>
2023-05-15 00:19:24 +0200

    Adding db helper

---
+++ app/src/main/java/com/example/scarlet/ScarletDbHelper.kt
@@ -0,0 +1,33 @@
+package com.example.scarlet
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+private const val TAG = "ScarletDbHelper"
+class ScarletDbHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    private var db: SQLiteDatabase? = null
+
+    override fun onCreate(db: SQLiteDatabase) {
+        this.db = db
+        Log.i(TAG, "Creating the database schema")
+        db.execSQL(CREATE_SCHEMA_QUERY)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        Log.i(TAG, "Dropping and recreating the database schema")
+        db!!.execSQL(DROP_SCHEMA_QUERY)
+        this.onCreate(db)
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "scarlet"
+        private const val DATABASE_VERSION = 1
+        private const val CREATE_SCHEMA_QUERY = "CREATE TABLE block(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, completed BOOLEAN)"
+        private const val DROP_SCHEMA_QUERY = "DROP TABLE IF EXISTS block"
+    }
+
+}
