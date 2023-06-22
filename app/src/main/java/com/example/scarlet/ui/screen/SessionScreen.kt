d31ad90c61e92e780faf1bd5247a55268ff68769
Reda AGAOUA <reda.agaoua@gmail.com>
2023-06-22 18:46:29 +0200

    Corrected missing import for modifier.background

--- app/src/main/java/com/example/scarlet/ui/screen/SessionScreen.kt
+++ app/src/main/java/com/example/scarlet/ui/screen/SessionScreen.kt
@@ -1,11 +1,11 @@
 package com.example.scarlet.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
