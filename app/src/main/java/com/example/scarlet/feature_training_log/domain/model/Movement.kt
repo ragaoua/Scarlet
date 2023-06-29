5caf9f515c2969160654c6013946f258eb199da7
Reda AGAOUA <reda.agaoua@gmail.com>
2023-06-29 21:22:53 +0200

    Movement attributes now have default values

--- app/src/main/java/com/example/scarlet/feature_training_log/domain/model/Movement.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/domain/model/Movement.kt
@@ -6,6 +6,6 @@ import androidx.room.PrimaryKey
 @Entity
 data class Movement(
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val name: String
+    val id: Int = 0,
+    val name: String = ""
 )
