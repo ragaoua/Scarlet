2fe146d67f8dbbf1c3f363ce4b1e7a408351a04b
Reda AGAOUA <reda.agaoua@gmail.com>
2023-07-10 18:39:56 +0200

    Selected a new color scheme for material3

--- app/src/main/java/com/example/scarlet/ui/theme/Color.kt
+++ app/src/main/java/com/example/scarlet/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.example.scarlet.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green10 = Color(0xFF0c2715)
+val Green20 = Color(0xFF184e2a)
+val Green30 = Color(0xFF25743f)
+val Green40 = Color(0xFF319b54)
+val Green80 = Color(0xFFb1e7c3)
+val Green90 = Color(0xFFd8f3e1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkGreen10 = Color(0xFF151e18)
+val DarkGreen20 = Color(0xFF2a3c31)
+val DarkGreen30 = Color(0xFF405a4a)
+val DarkGreen40 = Color(0xFF557762)
+val DarkGreen80 = Color(0xFFc3d5ca)
+val DarkGreen90 = Color(0xFFe1eae4)
+
+val Orange10 = Color(0xFF331400)
+val Orange20 = Color(0xFF662900)
+val Orange30 = Color(0xFF993d00)
+val Orange40 = Color(0xFFcc5200)
+val Orange80 = Color(0xFFffc299)
+val Orange90 = Color(0xFFffe0cc)
+
+val Red10 = Color(0xFF2f0406)
+val Red20 = Color(0xFF5e080c)
+val Red30 = Color(0xFF8e0b12)
+val Red40 = Color(0xFFbd0f18)
+val Red80 = Color(0xFFf7a1a5)
+val Red90 = Color(0xFFfbd0d2)
