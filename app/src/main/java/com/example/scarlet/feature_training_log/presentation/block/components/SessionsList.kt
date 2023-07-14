77e101c270b7c1c2d03d3492c5220bcadf1fc456
Reda AGAOUA <reda.agaoua@gmail.com>
2023-07-14 11:38:47 +0200

    Adapted sessionList to use material3 design
    Commented out the onDelete (to refactor later)

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/block/components/SessionsList.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/block/components/SessionsList.kt
@@ -3,18 +3,21 @@ package com.example.scarlet.feature_training_log.presentation.block.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.scarlet.R
 import com.example.scarlet.feature_training_log.domain.model.Session
 import com.example.scarlet.feature_training_log.presentation.block.BlockEvent
-import com.example.scarlet.feature_training_log.presentation.components.ScarletListItem
 import com.example.scarlet.feature_training_log.presentation.components.TitledLazyList
 import com.example.scarlet.feature_training_log.presentation.destinations.SessionScreenDestination
+import com.example.scarlet.ui.theme.MainButtonContentPadding
+import com.example.scarlet.ui.theme.TitleLazyListPadding
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
@@ -28,19 +31,29 @@ fun SessionsList(
     TitledLazyList(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 24.dp, end = 24.dp),
+            .padding(TitleLazyListPadding),
         title = stringResource(R.string.block_sessions_list_title)
     ) {
         items(sessions) { session ->
-            ScarletListItem(
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = MainButtonContentPadding,
+                shape = MaterialTheme.shapes.large,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.onSecondary
+                ),
                 onClick = {
                     navigator.navigate(SessionScreenDestination(session = session))
-                },
-                onDelete = {
-                    onEvent(BlockEvent.DeleteSession(session))
                 }
+//                onDelete = {
+//                    onEvent(BlockEvent.DeleteSession(session))
+//                } /* TODO */
             ) {
-                Text(text = session.date)
+                Text(
+                    text = session.date,
+                    style = MaterialTheme.typography.titleLarge
+                )
             }
         }
     }
