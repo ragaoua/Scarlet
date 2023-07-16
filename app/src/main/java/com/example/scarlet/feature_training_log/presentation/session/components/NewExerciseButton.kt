4529a3a6c6524331e6cf9c35a0d912d1c25d7384
Reda AGAOUA <reda.agaoua@gmail.com>
2023-07-16 22:12:38 +0200

    NewExerciseButton now implemented as an m3 button

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/NewExerciseButton.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/NewExerciseButton.kt
@@ -1,43 +1,32 @@
 package com.example.scarlet.feature_training_log.presentation.session.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import com.example.scarlet.R
 import com.example.scarlet.feature_training_log.presentation.session.SessionEvent
 
 @Composable
 fun NewExerciseButton(
-    modifier: Modifier = Modifier,
     onEvent: (SessionEvent) -> Unit
 ) {
-    Box(
-        modifier = modifier
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .border(BorderStroke(2.dp, MaterialTheme.colorScheme.onSurface))
-                .clickable {
-                    onEvent(SessionEvent.ShowNewExerciseDialog)
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "New Exercise" /* TODO: create a string resource */
-            )
+    Button(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.extraSmall,
+        onClick = {
+            onEvent(SessionEvent.ShowNewExerciseDialog)
         }
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = stringResource(R.string.new_exercise)
+        )
     }
 }
