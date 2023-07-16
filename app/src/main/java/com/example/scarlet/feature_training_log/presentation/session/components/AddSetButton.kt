6d61d7da67920646cbb27a2a3b3966fd9f4d4227
Reda AGAOUA <reda.agaoua@gmail.com>
2023-07-16 22:44:03 +0200

    AddSetButton : implemented as an m3 outlined button

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/AddSetButton.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/AddSetButton.kt
@@ -1,47 +1,45 @@
 package com.example.scarlet.feature_training_log.presentation.session.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.scarlet.R
 import com.example.scarlet.feature_training_log.domain.model.Exercise
 import com.example.scarlet.feature_training_log.presentation.session.SessionEvent
 
 @Composable
 fun AddSetButton(
-    modifier: Modifier = Modifier,
     exercise: Exercise,
     onEvent: (SessionEvent) -> Unit
 ) {
-    Box(
-        modifier = modifier
+    // Overriding the defaultMinSize and contentPadding to make the button smaller
+    // This is against Material Design guidelines, but it looks/feels better for the UI
+    OutlinedButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+            .defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
+        contentPadding = PaddingValues(0.dp),
+        shape = MaterialTheme.shapes.extraSmall,
+        onClick = {
+            onEvent(SessionEvent.NewSet(exercise))
+        }
     ) {
-       Box(
-           modifier = Modifier
-               .fillMaxWidth()
-               .padding(8.dp)
-               .border(BorderStroke(2.dp, MaterialTheme.colorScheme.onSurface))
-               .clickable {
-                   onEvent(SessionEvent.NewSet(exercise))
-               },
-           contentAlignment = Alignment.Center
-       ) {
-           Icon(
-               imageVector = Icons.Default.Add,
-               contentDescription = "Add Set" /* TODO: create a string resource */
-           )
-       }
+       Icon(
+           imageVector = Icons.Default.Add,
+           contentDescription = stringResource(R.string.add_set)
+       )
     }
 }
 
