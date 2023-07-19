6929338367d10f4652ad00db4abac077e2126fb9
Reda AGAOUA <reda.agaoua@gmail.com>
2023-07-19 23:07:49 +0200

    New core composable : SecondaryActionButton

---
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/components/SecondaryActionButton.kt
@@ -0,0 +1,28 @@
+package com.example.scarlet.feature_training_log.presentation.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SecondaryActionButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    // Overriding the defaultMinSize and contentPadding to make the button smaller
+    // This is against Material Design guidelines, but it looks/feels better for the UI
+    OutlinedButton(
+        modifier = modifier
+            .defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
+        contentPadding = PaddingValues(0.dp),
+        shape = MaterialTheme.shapes.extraSmall,
+        onClick = onClick,
+        content = content
+    )
+}
--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/AddSetButton.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/components/AddSetButton.kt
@@ -1,14 +1,10 @@
 package com.example.scarlet.feature_training_log.presentation.session.components
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -16,6 +12,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.scarlet.R
 import com.example.scarlet.feature_training_log.domain.model.Exercise
+import com.example.scarlet.feature_training_log.presentation.components.SecondaryActionButton
 import com.example.scarlet.feature_training_log.presentation.session.SessionEvent
 
 @Composable
@@ -23,15 +20,10 @@ fun AddSetButton(
     exercise: Exercise,
     onEvent: (SessionEvent) -> Unit
 ) {
-    // Overriding the defaultMinSize and contentPadding to make the button smaller
-    // This is against Material Design guidelines, but it looks/feels better for the UI
-    OutlinedButton(
+    SecondaryActionButton(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 8.dp)
-            .defaultMinSize(minHeight = 1.dp, minWidth = 1.dp),
-        contentPadding = PaddingValues(0.dp),
-        shape = MaterialTheme.shapes.extraSmall,
+            .padding(horizontal = 8.dp),
         onClick = {
             onEvent(SessionEvent.NewSet(exercise))
         }
