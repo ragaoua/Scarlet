3385179266b134f7d64a124ffd89084a7abd47ec
Reda AGAOUA <reda.agaoua@gmail.com>
2023-08-08 18:41:31 +0200

    DeleteBlockUseCase doesnt need to return anything

--- app/src/main/java/com/example/scarlet/feature_training_log/domain/use_case/training_log/DeleteBlockUseCase.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/domain/use_case/training_log/DeleteBlockUseCase.kt
@@ -1,7 +1,5 @@
 package com.example.scarlet.feature_training_log.domain.use_case.training_log
 
-import com.example.scarlet.core.util.Resource
-import com.example.scarlet.core.util.SimpleResource
 import com.example.scarlet.feature_training_log.domain.model.Block
 import com.example.scarlet.feature_training_log.domain.repository.ScarletRepository
 
@@ -9,9 +7,8 @@ class DeleteBlockUseCase(
     private val repository: ScarletRepository
 ) {
 
-    suspend operator fun invoke(block: Block): SimpleResource {
+    suspend operator fun invoke(block: Block) {
         repository.deleteBlock(block)
-        return Resource.Success()
     }
 
 }
