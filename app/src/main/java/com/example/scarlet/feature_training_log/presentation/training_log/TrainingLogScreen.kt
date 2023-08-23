a05a2511c3c3e97e9484d15f3e68a770f05695de
Reda AGAOUA <reda.agaoua@gmail.com>
2023-08-23 23:23:58 +0200

    Made the "training log" screen title into a topAppBAr

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/training_log/TrainingLogScreen.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/training_log/TrainingLogScreen.kt
@@ -1,28 +1,30 @@
 package com.example.scarlet.feature_training_log.presentation.training_log
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -51,6 +53,7 @@ fun TrainingLogScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen(
     navigator: DestinationsNavigator,
@@ -82,7 +85,11 @@ fun Screen(
      * Actual screen
      ************************************************************************/
     ScarletTheme {
+        val topAppBarScrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
         Scaffold(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection),
             snackbarHost = { SnackbarHost(snackbarHostState) },
             floatingActionButton = {
                 FloatingActionButton(
@@ -93,32 +100,36 @@ fun Screen(
                         contentDescription = stringResource(id = R.string.add_new_block)
                     )
                 }
+            },
+            topBar = {
+                LargeTopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.training_log),
+                            style = MaterialTheme.typography.displaySmall,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    },
+                    colors = TopAppBarDefaults.largeTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.background
+                    ),
+                    scrollBehavior = topAppBarScrollBehavior,
+                )
             }
         ) { innerPadding ->
             Surface (
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(innerPadding),
+                    .padding(innerPadding)
+                    .padding(top = 32.dp),
                 color = MaterialTheme.colorScheme.background
             ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Spacer(modifier = Modifier.height(64.dp))
-                    Text(
-                        text = stringResource(id = R.string.training_log),
-                        style = MaterialTheme.typography.displayMedium
-                    )
-
-                    Spacer(modifier = Modifier.height(32.dp))
-
-                    BlockListSection(
-                        navigator = navigator,
-                        blocks = state.blocks,
-                        onEvent = onEvent
-                    )
-                }
+                BlockListSection(
+                    navigator = navigator,
+                    blocks = state.blocks,
+                    onEvent = onEvent
+                )
             }
         }
         state.newBlockSheetState?.let {
