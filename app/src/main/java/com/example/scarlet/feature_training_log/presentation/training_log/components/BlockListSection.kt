ce24d858ff814f4460d7c66ab2850fbb2f0b2f1b
Reda AGAOUA <reda.agaoua@gmail.com>
2023-08-23 19:56:16 +0200

    Now displaying blocks as 2 sections : latest then preceding

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/training_log/components/BlockListSection.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/training_log/components/BlockListSection.kt
@@ -1,8 +1,10 @@
 package com.example.scarlet.feature_training_log.presentation.training_log.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -13,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.scarlet.R
 import com.example.scarlet.feature_training_log.domain.model.BlockWithSessions
 import com.example.scarlet.feature_training_log.presentation.core.components.DeletableItem
@@ -30,62 +33,128 @@ fun BlockListSection(
     blocks: List<BlockWithSessions> = emptyList(),
     onEvent: (TrainingLogEvent) -> Unit
 ) {
-    TitledLazyList(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(TitleLazyListPadding),
-        title = stringResource(R.string.training_blocks)
+    /*************************************************************************
+     * Latest training block
+     *************************************************************************/
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (blocks.isNotEmpty()) {
-            items(blocks) {
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentPadding = MainButtonContentPadding,
-                    shape = MaterialTheme.shapes.large,
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.secondary,
-                        contentColor = MaterialTheme.colorScheme.onSecondary
-                    ),
-                    onClick = {
-                        navigator.navigate(BlockScreenDestination(it.block))
-                    }
-                ) {
-                    DeletableItem (
-                        modifier = Modifier.fillMaxSize(),
-                        onDeleteClicked = {
-                            onEvent(TrainingLogEvent.DeleteBlock(it.block))
+
+        TitledLazyList(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(TitleLazyListPadding),
+            title = stringResource(R.string.latest_training_block),
+        ) {
+            blocks.firstOrNull()?.let { latestBlockWithSessions ->
+                item(latestBlockWithSessions) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = MainButtonContentPadding,
+                        shape = MaterialTheme.shapes.large,
+                        onClick = {
+                            navigator.navigate(BlockScreenDestination(latestBlockWithSessions.block))
                         }
                     ) {
-                        Column(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally
+                        DeletableItem(
+                            modifier = Modifier.fillMaxSize(),
+                            onDeleteClicked = {
+                                onEvent(TrainingLogEvent.DeleteBlock(latestBlockWithSessions.block))
+                            }
                         ) {
-                            Text(
-                                text = it.block.name,
-                                style = MaterialTheme.typography.titleLarge
-                            )
-                            Text(
-                                text =
-                                if (it.sessions.isNotEmpty()) {
-                                    DateUtils.formatDate(it.sessions.first().date) + " - " +
-                                            DateUtils.formatDate(it.sessions.last().date)
-                                } else {
-                                    stringResource(R.string.empty_block)
-                                },
-                                style = MaterialTheme.typography.bodyMedium,
-                            )
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    text = latestBlockWithSessions.block.name,
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                                Text(
+                                    text =
+                                    if (latestBlockWithSessions.sessions.isNotEmpty()) {
+                                        stringResource(
+                                            R.string.block_started_on,
+                                            DateUtils.formatDate(latestBlockWithSessions.sessions.first().date)
+                                        )
+                                    } else {
+                                        stringResource(R.string.empty_block)
+                                    },
+                                    style = MaterialTheme.typography.bodyLarge
+                                )
+                            }
                         }
                     }
-
+                }
+            } ?: run {
+                item {
+                    Text(
+                        text = stringResource(R.string.no_training_blocks),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Text(
+                        text = stringResource(R.string.start_new_block),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
                 }
             }
-        } else {
-            item {
-                Text(
-                    text = stringResource(R.string.no_training_blocks),
-                    style = MaterialTheme.typography.bodyMedium
-                    // TODO color = grey
-                )
+        }
+
+        Spacer(Modifier.height(16.dp))
+
+        /*************************************************************************
+         * Preceding training blocks
+         *************************************************************************/
+        if (blocks.size > 1) {
+            TitledLazyList(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(TitleLazyListPadding),
+                title = stringResource(R.string.preceding_training_blocks)
+            ) {
+                items(blocks.subList(1, blocks.size)) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = MainButtonContentPadding,
+                        shape = MaterialTheme.shapes.large,
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = MaterialTheme.colorScheme.onSecondary
+                        ),
+                        onClick = {
+                            navigator.navigate(BlockScreenDestination(it.block))
+                        }
+                    ) {
+                        DeletableItem(
+                            modifier = Modifier.fillMaxSize(),
+                            onDeleteClicked = {
+                                onEvent(TrainingLogEvent.DeleteBlock(it.block))
+                            }
+                        ) {
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    text = it.block.name,
+                                    style = MaterialTheme.typography.titleLarge
+                                )
+                                Text(
+                                    text =
+                                    if (it.sessions.isNotEmpty()) {
+                                        DateUtils.formatDate(it.sessions.first().date) + " - " +
+                                                DateUtils.formatDate(it.sessions.last().date)
+                                    } else {
+                                        stringResource(R.string.empty_block)
+                                    },
+                                    style = MaterialTheme.typography.bodyMedium,
+                                )
+                            }
+                        }
+
+                    }
+                }
             }
         }
     }
