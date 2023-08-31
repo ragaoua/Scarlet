6f0f263b5221a97331dc3d2dee2a34b27addaf9d
Reda AGAOUA <reda.agaoua@gmail.com>
2023-08-31 16:48:09 +0200

    #11 : deleted the session screen as it is not used anymore

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/SessionScreenNavArgs.kt
+++
@@ -1,9 +0,0 @@
-package com.example.scarlet.feature_training_log.presentation.session
-
-import com.example.scarlet.feature_training_log.domain.model.Block
-import com.example.scarlet.feature_training_log.domain.model.Session
-
-data class SessionScreenNavArgs(
-    val session: Session,
-    val block: Block
-)
--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/session/util/SetFieldRatio.kt
+++
@@ -1,9 +0,0 @@
-package com.example.scarlet.feature_training_log.presentation.session.util
-
-object SetFieldRatio {
-    const val SET = 4f
-    const val REPS = 7f
-    const val WEIGHT = 9f
-    const val RPE = 8f
-    const val OTHER = 3f
-}
