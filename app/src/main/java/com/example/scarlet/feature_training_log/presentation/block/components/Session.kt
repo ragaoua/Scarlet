ad85575667a54cb63e197fd65302ae1b7944793c
Reda AGAOUA <reda.agaoua@gmail.com>
2023-10-09 20:10:11 +0200

    #58 : Added spacing at the end of the exercise list

--- app/src/main/java/com/example/scarlet/feature_training_log/presentation/block/components/Session.kt
+++ app/src/main/java/com/example/scarlet/feature_training_log/presentation/block/components/Session.kt
@@ -4,7 +4,9 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -143,6 +145,8 @@ fun Session(
                     )
                 }
             }
+
+            Spacer(modifier = Modifier.height(100.dp))
         }
     }
 }
